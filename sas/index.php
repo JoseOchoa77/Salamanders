0a895de2d42f3f633c4dc850f1aa0ca26f9a1b2e
JoseOchoa77 <2awesomejr@gmail.com>
2025-02-24 13:29:59 -0500

    init

---
+++ sas/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>SAS</title>
+    </head>
+
+    <body> 
+        <h1>Southern Appalachian Salamanders</h1>
+        <p>This is the home page for Southern Appalachian Salamanders.</p>
+        <p>I have put in links to each of the phases, so we can see how the project is progressing. Your requirements are different. Store all of your files for this project in just one folder, sas.</p>
+        <p>You are required to have a link or a redirect to sas/public/index.php in your sas/index.php file.</p>
+        <ul>
+            <li><a href="phase01">Phase01</li>
+            <li><a href="phase02">Phase02</li>
+            <li><a href="phase03">Phase03</li>
+            <li><a href="phase04">Phase04</li>
+            <li><a href="phase05">Phase05</li>
+        </ul>
+    </body>
+
+</html>
---
+++ sas/phase05/public/index.php
@@ -0,0 +1,15 @@
+<?php require_once('../private/initialize.php'); ?>
+
+<?php require_once('../private/shared/salamander-header.php'); ?>
+
+// Include a shared path to the salamander header
+<html>
+    <h1>Welcome to SAS</h1>
+    <p>The Southern Appalachian Mountains Region is often hailed as the Salamander Capital of the World. In fact, the Smithsonian Conservation Biology Institute proclaims that the Appalachian region is home to more salamander species than anywhere else in the world, making it a true hotspot for salamander biodiversity.</p> 
+    <p><a href="https://www.savethesalamanders.com/appalachian-salamanders/">Source</a></p>
+</html>
+
+<!-- Include a shared path to the salamander footer -->
+<?php require_once('../private/shared/salamander-footer.php'); ?>
+
+
