7c28397aa7dc05cbe09ed709d091263e6b2e7ecc
JoseOchoa77 <2awesomejr@gmail.com>
2025-03-10 15:03:56 -0400

    added create salamander option

--- phase02 files/phase02/public/salamanders/new.php
+++ phase02 files/phase02/public/salamanders/new.php
@@ -18,3 +18,19 @@ else {
     echo("No Error");
 }
 ?>
+
+<?php require_once('../../private/initialize.php'); 
+
+
+include(SHARED_PATH . '/salamander-header.php'); 
+?>
+
+<h2>Create Salamander</h2>
+<form action="<?= url_for('/salamanders/create.php');?>" method="post">
+    <label>Name</label><br>
+    <input type="text" name="salamander_name" value=""><br>
+    <input type="submit" value="edit salamander">
+</form>
+<p><a href="<?= url_for('/salamanders/index.php'); ?>">&laquo; Back to Salamander List</a></p>
+
+<?php include(SHARED_PATH . '/salamander-footer.php'); ?>
